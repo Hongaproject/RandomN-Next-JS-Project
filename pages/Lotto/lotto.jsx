1ad3df4e3dece496932dd45dbdf494139d5b8f02
Hongaproject <tkwlscjq99@gmail.com>
2023-07-13 20:37:15 +0900

    수정

--- pages/Lotto/lotto.jsx
+++ pages/Lotto/lotto.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import styled from "styled-components";
-import NumberBall from "../Lotto/Numberball";
+import NumberBall from "./numberball";
 
 
 function getNumbers(){
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react';
-import Lotto from '../pages/Lotto/Lotto';
+import Lotto from './Lotto/lotto';
 import styled from 'styled-components';
 
 export default function Home() {
