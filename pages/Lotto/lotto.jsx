31b61689b924d282de5a79b7410b0be78d9bc462
Hongaproject <tkwlscjq99@gmail.com>
2023-07-13 20:26:58 +0900

    수정

--- pages/Lotto/lotto.jsx
+++ pages/Lotto/lotto.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import NumberBall from "./numberBall";
 import styled from "styled-components";
+import NumberBall from "./Numberball";
 
 
 function getNumbers(){
